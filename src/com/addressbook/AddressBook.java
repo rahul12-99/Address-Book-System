243679c08d9eb4ca376eae8c86bdb186e764d0d1
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-26 07:44:08 +0530

    Added : Code for add contact in addressBook

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,22 +1,43 @@
 package com.addressbook;
 
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class AddressBook {
 
-    public void createAddressBook(){
-        ContactPerson contactPerson = new ContactPerson();
-        contactPerson.setFirstName("Rahul");
-        contactPerson.setLastName("Kumar");
-        contactPerson.setAddress("Banka");
-        contactPerson.setCity("Patna");
-        contactPerson.setState("Bihar");
-        contactPerson.setZip(813211);
-        contactPerson.setPhoneNumber(993469189);
-        contactPerson.setEmail("rahul@123");
-        System.out.println(contactPerson);
+    ArrayList<ContactPerson> contact = new ArrayList<>();
+
+    public void addContact() { // Method to add contact in addressBook;
+        System.out.println("Enter How many contacts you want to add");
+        Scanner sc = new Scanner(System.in);
+        int noOfContact = sc.nextInt();
+        for (int i = 1; i <= noOfContact; i++) {
+            ContactPerson contactPerson = new ContactPerson();
+            System.out.println("Enter First name of " + i + " " + "contact");
+            contactPerson.setFirstName(sc.next());
+            System.out.println("Enter Last name of " + i + " " + "contact");
+            contactPerson.setLastName(sc.next());
+            System.out.println("Enter Address of " + i + " " + "contact");
+            contactPerson.setAddress(sc.next());
+            System.out.println("Enter City of " + i + " " + " contact");
+            contactPerson.setCity(sc.next());
+            System.out.println("Enter State of " + i + " " + "contact");
+            contactPerson.setState(sc.next());
+            System.out.println("Enter Zip code of " + i + " " + "contact");
+            contactPerson.setZip(sc.nextInt());
+            System.out.println("Enter Phone no of " + i + " " + "contact");
+            contactPerson.setPhoneNumber(sc.nextLong());
+            System.out.println("Enter Email of " + i + " " + "contact");
+            contactPerson.setEmail(sc.next());
+            // Add details in ArrayList;
+            contact.add(contactPerson);
+        }
+        System.out.println(contact);
     }
 
     public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
-        addressBook.createAddressBook();
+        addressBook.addContact();
     }
 }
